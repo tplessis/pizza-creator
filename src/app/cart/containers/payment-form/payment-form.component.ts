064a1f3377f52d1addfd141a02fa9bb5818198de
Thomas Plessis <thomas.plessis@insidegroup.fr>
2021-11-25 13:33:32 +0100

    fix(cart): fix confettti configuration

--- src/app/cart/containers/payment-form/payment-form.component.ts
+++ src/app/cart/containers/payment-form/payment-form.component.ts
@@ -54,16 +54,39 @@ export class PaymentFormComponent implements OnInit {
     confettiCanvas.style.top = '0';
     document.body.appendChild(confettiCanvas);
 
+    const conf = confetti.create(confettiCanvas);
     const count = 200;
     const defaults = {
       origin: { y: 0.7 }
     };
 
     const fire = (particleRatio, opts) => {
-      confetti.create(confettiCanvas)(Object.assign({}, defaults, opts, {
+      conf(Object.assign({}, defaults, opts, {
         particleCount: Math.floor(count * particleRatio)
       }));
     }
+
+    fire(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+
+    fire(0.2, {
+      spread: 60,
+    });
+
+    fire(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8
+    });
+
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2
+    });
 
     fire(0.25, {
       spread: 200,
